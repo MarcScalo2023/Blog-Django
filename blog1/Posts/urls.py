d43d0ac8623cf89e7004b8c1e9c8e2cc6982d458
MarcScalo2023 <ovando_43@hotmail.com>
2024-05-05 05:37:06 -0300

    html-v2

--- blog1/Posts/urls.py
+++ blog1/Posts/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.posts_page, name='posts_page'),
+    path('', views.home,name='home'),
+]
--- blog1/Posts/views.py
+++ blog1/Posts/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'Posts/posts_page.html')
-    
+
+
+def posts_page(request):
+    posts = Post.objects.all()
+    return render(request, 'Posts/posts_page.html', {'posts': posts})
