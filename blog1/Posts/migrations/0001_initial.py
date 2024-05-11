d6c5bd83ae2e48c52041894d3f7895ea1ef42b4c
Silvio Matorras <103513259+msilvio12@users.noreply.github.com>
2024-05-11 02:59:42 -0300

    cambiosf4

--- blog1/Posts/migrations/0001_initial.py
+++ blog1/Posts/migrations/0001_initial.py
@@ -1,1 +1,37 @@
+from django.db import migrations, models
+import django.utils.timezone
 
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Posts', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='post',
+            options={'verbose_name': 'entrada', 'verbose_name_plural': 'entradas'},
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='created',
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='modified',
+            field=models.DateTimeField(auto_now=True),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='content',
+            field=models.TextField(verbose_name='Contenido'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='title',
+            field=models.CharField(max_length=200, verbose_name='Título'),
+        ),
+    ]
