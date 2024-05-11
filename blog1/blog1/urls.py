299d3c8bbb07d88adcfc8ea686d4191edd07e294
Silvio Matorras <103513259+msilvio12@users.noreply.github.com>
2024-05-11 03:06:40 -0300

    cambiosf4

--- blog1/blog1/urls.py
+++ blog1/blog1/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Posts.views import home, post
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', home, name="home"),
+    path('entrada/<id>', post, name="post"),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
